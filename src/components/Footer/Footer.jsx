bbfaada1ef31f65ac5002855e1e78ff65c65d71f
Shivansh-OP <shivanshsoni327@outlook.com>
2024-07-17 01:16:23 +0530

    footer completed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import logo from '../../assets/logo.png'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom';
+import { Button } from '../index';
+import { Github, Mail, Twitter } from 'lucide-react';
 
 export function Footer() {
     
@@ -9,11 +12,112 @@ export function Footer() {
     return (
         <>
             <footer>
-                <div className='flex items-center justify-center'>
-                    <hr className='w-11/12 border border-primaryColor'/>
+                <hr className={`border 
+                    ${currentTheme === 'dark' ? 'border-secondaryColor' : 'border-primaryColor'}`}
+                />
+                <div className='space-x-6 my-5 sm:flex sm:justify-around sm:items-start sm:space-x-60 sm:my-7'>
+                    <div className='flex justify-center items-center space-x-2'>
+                        <img src={logo} className='h-14 w-auto sm:h-16' alt='Logo' />
+                        <h1 className={`font-customFont text-3xl
+                            ${currentTheme === 'dark' ? 'text-whiteColor' : 'text-blackColor'}`}
+                        >
+                            No <span className={`${currentTheme === 'dark' ? 'text-secondaryColor' : 'text-primaryColor'}`}>Cost</span> UC
+                        </h1>
+                    </div>
+                    <div className='sm:flex sm:justify-center sm:items-start sm:space-x-36'>
+                        <div className='hidden sm:block'>
+                            <h2 className={`font-customFont text-2xl underline my-2.5
+                                ${currentTheme === 'dark' ? 'text-whiteColor' : 'text-blackColor'}`}>
+                                Products
+                            </h2>
+                            <ul>
+                                <li className='mb-0.5'>
+                                    <Link to='/uc-purchase'
+                                        className={`font-customFont text-xl
+                                        ${currentTheme === 'dark' ? 'text-whiteColor hover:text-secondaryColor' : 'text-blackColor hover:text-primaryColor' }`}
+                                    >
+                                        UC Purchase
+                                    </Link>
+                                </li>
+                                <li className='mb-0.5'>
+                                    <Link to='/upgradable-guns'
+                                        className={`font-customFont text-xl
+                                        ${currentTheme === 'dark' ? 'text-whiteColor hover:text-secondaryColor' : 'text-blackColor hover:text-primaryColor' }`}
+                                    >
+                                        Upgradable Guns
+                                    </Link>
+                                </li>
+                                <li className='mb-0.5'>
+                                    <Link to='/super-cars'
+                                        className={`font-customFont text-xl
+                                        ${currentTheme === 'dark' ? 'text-whiteColor hover:text-secondaryColor' : 'text-blackColor hover:text-primaryColor' }`}
+                                    >
+                                        Super Cars
+                                    </Link>
+                                </li>
+                                <li className='mb-0.5'>
+                                    <Link to='/max-suits'
+                                        className={`font-customFont text-xl
+                                        ${currentTheme === 'dark' ? 'text-whiteColor hover:text-secondaryColor' : 'text-blackColor hover:text-primaryColor' }`}
+                                    >
+                                        Max Suits
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className='text-center sm:pr-32 sm:text-left'>
+                            <h2 className={`font-customFont text-2xl underline my-2.5
+                                    ${currentTheme === 'dark' ? 'text-whiteColor' : 'text-blackColor'}`}>
+                                    Legals
+                            </h2>
+                            <ul>
+                                <li className='mb-0.5'>
+                                    <Link to='/terms-and-conditions'
+                                            className={`font-customFont text-xl
+                                            ${currentTheme === 'dark' ? 'text-whiteColor hover:text-secondaryColor' : 'text-blackColor hover:text-primaryColor' }`}
+                                        >
+                                            Terms &amp; Conditions
+                                    </Link>
+                                </li>
+                                <li className='mb-0.5'>
+                                    <Link to='/privacy-policy'
+                                            className={`font-customFont text-xl
+                                            ${currentTheme === 'dark' ? 'text-whiteColor hover:text-secondaryColor' : 'text-blackColor hover:text-primaryColor' }`}
+                                        >
+                                            Privacy Policy
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-                <div  className='ml-28'>
-                    <img src={logo} className='h-16 w-auto mt-1' alt='Logo'/>
+                <div className='flex justify-center items-center'>
+                    <hr className={`border w-4/5
+                        ${currentTheme === 'dark' ? 'border-secondaryColor' : 'border-primaryColor'}`}
+                    />
+                </div>
+                <div className='my-5 sm:flex sm:justify-around sm:items-center sm:space-x-96'>
+                    <h3 className={`font-customFont text-lg text-center
+                        ${currentTheme === 'dark' ? 'text-whiteColor' : 'text-blackColor'}`}>
+                        &copy; Copyright 2024. All Rights Reserved by No <span className={`${currentTheme === 'dark' ? 'text-secondaryColor' : 'text-primaryColor'}`}>Cost</span> UC.
+                    </h3>
+                    <div className='flex justify-center items-center space-x-6 my-5'>
+                        <Button className='rounded-full p-3'>
+                            <Mail size={24} strokeWidth={1.5}
+                                className={`${currentTheme === 'dark' ? 'text-whiteColor' : 'text-blackColor'}`}
+                            />
+                        </Button>
+                        <Button className='rounded-full p-3'>
+                            <Twitter size={24} strokeWidth={1.5}
+                                className={`${currentTheme === 'dark' ? 'text-whiteColor' : 'text-blackColor'}`}
+                            />
+                        </Button>
+                        <Button className='rounded-full p-3'>
+                            <Github size={24} strokeWidth={1.5}
+                                className={`${currentTheme === 'dark' ? 'text-whiteColor' : 'text-blackColor'}`}
+                            />
+                        </Button>
+                    </div>
                 </div>
             </footer>
         </>
