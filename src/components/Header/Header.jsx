c6dd71c20f55437c86d0524f9b0a2e8e8016d5c1
Shivansh-OP <shivanshsoni327@outlook.com>
2024-07-24 19:13:05 +0530

    header completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import logo from '../../assets/logo.png'
 import { Button, Container, ThemeChanger } from '../index'
@@ -30,6 +30,21 @@ export function Header() {
     ]
     
     const [showDropdown, setShowDropdown] = useState(false);
+    const dropdownRef = useRef(null);
+    
+    useEffect(() => {
+        const handleDropdown = (event) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setShowDropdown(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleDropdown);
+        
+        return () => {
+            document.addEventListener('mousedown', handleDropdown);
+        };
+    }, [])
 
     const openDropdown = () => {
         setShowDropdown(!showDropdown);
@@ -44,14 +59,19 @@ export function Header() {
             <header>
                 <Container>
                     <nav className={`flex justify-center items-center space-x-28 pb-2 fixed top-0 left-0 right-0 z-50 sm:p-4 sm:justify-evenly sm:space-x-72
-                        ${currentTheme === 'dark' ? 'bg-blackColor text-whiteColor' : 'bg-whiteColor text-blackColor'}`}>
+                        ${currentTheme === 'dark' ? 'bg-blackColor text-whiteColor' : 'bg-whiteColor text-blackColor'}`}
+                    >
                         <Link to='/'>
                             <div className='flex justify-center items-center space-x-2 mt-4 sm:mt-1'>
                                 <img src={logo} className='h-14 w-auto sm:h-16 ' alt='Logo'/>
                                 <h1 className={`font-customFont text-3xl 
                                     ${currentTheme === 'dark' ? 'text-whiteColor' : 'text-blackColor'}`}
                                 >
-                                    No <span className={`${currentTheme === 'dark' ? 'text-secondaryColor' : 'text-primaryColor'}`}>Cost</span> UC
+                                    No {' '}
+                                    <span className={`${currentTheme === 'dark' ? 'text-secondaryColor' : 'text-primaryColor'}`}>
+                                        Cost
+                                    </span> {' '}
+                                    UC
                                 </h1>
                             </div>
                         </Link>
@@ -71,10 +91,12 @@ export function Header() {
                                     />
                                 )
                             }
-                            <div className='flex justify-end items-start'>
-                                <div className={`sm:flex sm:items-center sm:space-x-6 sm:bg-transparent
-                                    ${showDropdown ? 'absolute w-40 text-center mt-7 p-3 rounded-lg' : 'hidden'}
-                                    ${currentTheme === 'dark' ? 'bg-whiteColor' : 'bg-blackColor'}`}
+                            <div className='flex justify-center items-start'>
+                                <div ref={dropdownRef}
+                                    className={`left-1/2 transform -translate-x-1/2
+                                        sm:flex sm:items-center sm:space-x-6 sm:bg-transparent sm:transform-none
+                                        ${showDropdown ? 'absolute w-11/12 text-center mt-10 p-3 rounded-lg' : 'hidden'}
+                                        ${currentTheme === 'dark' ? 'bg-whiteColor text-blackColor sm:text-whiteColor' : 'bg-blackColor text-whiteColor sm:text-blackColor'}`}
                                 >
                                     {navbarItems.map((item) => (
                                         <li key={item.name}>
@@ -82,7 +104,7 @@ export function Header() {
                                                 navigate(item.url);
                                                 closeDropdown();
                                             }}
-                                            className='rounded-lg text-xl w-32 py-1 my-1.5 hover:bg-secondaryColor active:bg-secondaryColor
+                                            className='rounded-lg text-xl w-64 py-1.5 my-1.5 hover:bg-secondaryColor active:bg-secondaryColor
                                                 sm:w-auto sm:px-7 sm:py-2'
                                             >
                                                 {item.name}
